842e02706619fec6d1ca49bc0b0cb2f62025b94a
chriszandro_rrze <chriszandro@gmail.com>
2015-07-15 14:43:08 +0200

    Bugs behoben

--- job_delete.py
+++ job_delete.py
@@ -1,8 +1,8 @@
 import subprocess
 
-index_start = 1716244
-index_end = 1716316
+index_start = 395323
+index_end = 395361
 
 for i in range(index_start, index_end):
     command_string = "qdel"
-    subprocess.call([command_string,str(i)], shell=False)
+    subprocess.call([command_string, str(i)], shell=False)
