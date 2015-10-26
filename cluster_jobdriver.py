966973bd4894f5c05fd6bffe60e86d38047948f8
chriszandro_rrze <chriszandro@gmail.com>
2015-10-26 19:00:13 +0100

    cluster driver new job added: switch_lowT

--- cluster_jobdriver.py
+++ cluster_jobdriver.py
@@ -174,11 +174,11 @@ def symmetric_gate_U(Fieldlength=10, delta=0.1, transloc=1):
 # In[4]:
 
 ### Dynamic
-system_l025_dyn = {"l":0.25, "delta":0.1, "gate":[0, 1.8, 2.8] , "frank":[0.0], "barrier":[0.2, 0.5], 
+system_l025_dyn = {"l":0.25, "delta":0.1, "gate":[0, 1.8, 2.8] , "frank":[-0.1, 0.0], "barrier":[0.2, 0.5], 
                "operation":[], "A":0.1, "B":1, "C":7}  
-system_l05_dyn = {"l":0.5, "delta":0.1,"gate":[0, 0.8, 2.8] , "frank":[0.0, -0.3],"barrier":[0.2, 0.5], 
+system_l05_dyn = {"l":0.5, "delta":0.1,"gate":[0, 0.8, 2.8] , "frank":[-0.3, -0.1, 0.3, 0.0],"barrier":[0.2, 0.5], 
               "operation":[], "A":0.1, "B":1, "C":7}  
-system_l08_dyn = {"l":0.8, "delta":0.1, "gate":[0, 0.5, 2.8] , "frank":[0.0], "barrier":[0.2, 0.5], 
+system_l08_dyn = {"l":0.8, "delta":0.1, "gate":[0, 0.5, 2.8] , "frank":[-0.3, 0.0, 0.3], "barrier":[0.2, 0.5], 
               "operation":[], "A":0.1, "B":1, "C":7}
 configuration_dyn =[system_l025_dyn, system_l05_dyn, system_l08_dyn] 
 
@@ -213,32 +213,32 @@ for system in configuration_stat:
 # In[7]:
 
 #External Parameters
-temp =[10, 293] 
+temp =[293] 
 env =[0.0, 0.04] 
-bias = [0.2, 0.4]
+bias = [0.2, 0.4, 0.8]
 
 
 # In[8]:
-
-timestart = 0; timeend= 1e7; timegrid=1e5; clustertime_1 = "12:00:00"; cluster_1="lima"
+timestart = 0; timeend= 1e8; timegrid=1e5; clustertime_1 = "24:00:00"; cluster_1="lima"
 
 
 # ## off -> on
+########
 
 # In[27]:
 
 project_offon = cluster.jobproject(name="off-on", program=program_rrze, programprojectpath=path_rrze  , projectpath=projectpath,
-                                        mode=20, summary_bool=1, performance_bool=1, pop_bool=1,  N=2000, coupling_bool=1,
-                                        pop_number=15, rhox_bool=1, plot_bool=1, meBND=6, meBND_small=6,xranges=2.0e0,
-                                        medim1=60,medim0=60, timebool=0, potential_id=0)
+				mode=20, summary_bool=1, performance_bool=0, pop_bool=1,  N=2000, coupling_bool=1,
+				pop_number=15, rhox_bool=1, plot_bool=1, meBND=6, meBND_small=6,xranges=2.0e0,
+				medim1=60,medim0=60, timebool=0, potential_id=0)
 
 
 # In[28]:
 
 project_offon_rhox = cluster.jobproject(name="off-on_rhox", program=program_rrze, programprojectpath=path_rrze  , projectpath=projectpath,
-                                        mode=21, summary_bool=1, performance_bool=1, pop_bool=1,  N=2000, coupling_bool=1,
-                                        pop_number=15, rhox_bool=1, plot_bool=1, meBND=6, meBND_small=6,xranges=2.0e0,
-                                        medim1=60,medim0=60, timebool=0, potential_id=0)
+				mode=21, summary_bool=1, performance_bool=1, pop_bool=1,  N=2000, coupling_bool=1,
+				pop_number=15, rhox_bool=1, plot_bool=1, meBND=6, meBND_small=6,xranges=2.0e0,
+				medim1=60,medim0=60, timebool=0, potential_id=0)
 
 
 # ## on--> off
@@ -246,17 +246,17 @@ project_offon_rhox = cluster.jobproject(name="off-on_rhox", program=program_rrze
 # In[29]:
 
 project_onoff = cluster.jobproject(name="on-off", program=program_rrze, programprojectpath=path_rrze  , projectpath=projectpath,
-                                        mode=20, summary_bool=1, performance_bool=1, pop_bool=1,  N=2000, coupling_bool=1,
-                                        pop_number=15, rhox_bool=1, plot_bool=1, meBND=6, meBND_small=6,xranges=2.0e0,
-                                        medim1=60,medim0=60, timebool=0, potential_id=0)
+				mode=20, summary_bool=1, performance_bool=1, pop_bool=1,  N=2000, coupling_bool=1,
+				pop_number=15, rhox_bool=1, plot_bool=1, meBND=6, meBND_small=6,xranges=2.0e0,
+				medim1=60,medim0=60, timebool=0, potential_id=0)
 
 
 # In[30]:
 
 project_onoff_rhox = cluster.jobproject(name="on-off_rhox", program=program_rrze, programprojectpath=path_rrze  , projectpath=projectpath,
-                                        mode=21, summary_bool=1, performance_bool=1, pop_bool=1,  N=2000, coupling_bool=1,
-                                        pop_number=15, rhox_bool=1, plot_bool=1, meBND=6, meBND_small=6,xranges=2.0e0,
-                                        medim1=60,medim0=60, timebool=0, potential_id=0)
+				mode=21, summary_bool=1, performance_bool=1, pop_bool=1,  N=2000, coupling_bool=1,
+				pop_number=15, rhox_bool=1, plot_bool=1, meBND=6, meBND_small=6,xranges=2.0e0,
+				medim1=60,medim0=60, timebool=0, potential_id=0)
 
 
 # In[31]:
@@ -272,99 +272,100 @@ project_onoff_list = [project_onoff, project_onoff_rhox]
 # In[ ]:
 
 for potential in configuration_dyn:
-    for gate in potential["gate"]:
-        for frank in potential["frank"]:
-            for vb in potential["barrier"]:
-                ## Reservoir Parameters
-                    for b in bias:
-                        for en in env:
-                            for T in temp:
-                                
-                                    string = ""
-                                    string += human_readable_length(potential["l"])                                    
-                                    string += "_G_" + str(gate) +  "_Vb_" + str(vb) + "_B_" + str(b)                                                                     
-                                    string += human_readable_frank(frank)
-                                    string += human_readable_temperature(T)
-                                    string += human_readable_environment(env)
-                                    
-                                    for job in project_offon_list:
-                                        ## OFF -> ON ### ### ### ### ### ### ### ### ##
-                                        ## Job no rhox
-                                        job.add_job(
+ for gate in potential["gate"]:
+  for frank in potential["frank"]:
+    for vb in potential["barrier"]:
+        
+	## Reservoir Parameters
+	    for b in bias:
+		for en in env:
+		    for T in temp:
+			
+			    string = ""
+			    string += human_readable_length(potential["l"])                                    
+			    string += "_G_" + str(gate) +  "_Vb_" + str(vb) + "_B_" + str(b)                                                                     
+			    string += human_readable_frank(frank)
+			    string += human_readable_temperature(T)
+			    string += human_readable_environment(env)
+			    
+			    for job in project_offon_list:
+				## OFF -> ON ### ### ### ### ### ### ### ### ##
+				## Job no rhox
+				job.add_job(
 
-                                        #ClusterSpeccs
-                                        cluster=cluster_1,
-                                        time = clustertime_1,
+				#ClusterSpeccs
+				cluster=cluster_1,
+				time = clustertime_1,
 
-                                        specific=string, 
-                                        grid_bias_voltage = 1,
+				specific=string, 
+				grid_bias_voltage = 1,
 
-                                        ## Timing 
-                                        time_start=timestart,
-                                        time_end = timeend,
-                                        time_grid = timegrid,
+				## Timing 
+				time_start=timestart,
+				time_end = timeend,
+				time_grid = timegrid,
 
-                                        ## Fixed for this Project
-                                        C= potential["C"],
-                                        A =  potential["A"], B= potential["B"],
-                                        l = potential["l"],
-                                        delta= potential["delta"],
+				## Fixed for this Project
+				C= potential["C"],
+				A =  potential["A"], B= potential["B"],
+				l = potential["l"],
+				delta= potential["delta"],
 
-                                        beta2L = set_bath_by_temp(T), beta2R=set_bath_by_temp(T),
+				beta2L = set_bath_by_temp(T), beta2R=set_bath_by_temp(T),
 
-                                        #Loop Variables
-                                        ## Bias
-                                        start_bias_voltage=0.0,                                        
-                                        end_bias_voltage=b,
+				#Loop Variables
+				## Bias
+				start_bias_voltage=0.0,                                        
+				end_bias_voltage=b,
 
-                                        ## Gate
-                                        start_gate_voltage=gate,
-                                        end_gate_voltage=gate,
+				## Gate
+				start_gate_voltage=gate,
+				end_gate_voltage=gate,
 
-                                        Vb=vb,
-                                        xshift = frank,
-                                        T = T, hbath_temp=T,
-                                        eta = en)  
-                                        
-                                        
-                                    for job in project_onoff_list:
-                                        ## OFF -> ON ### ### ### ### ### ### ### ### ##
-                                        ## Job no rhox
-                                        job.add_job(
+				Vb=vb,
+				xshift = frank,
+				T = T, hbath_temp=T,
+				eta = en)  
+				
+				
+			    for job in project_onoff_list:
+				## OFF -> ON ### ### ### ### ### ### ### ### ##
+				## Job no rhox
+				job.add_job(
 
-                                        #ClusterSpeccs
-                                        cluster=cluster_1,
-                                        time = clustertime_1,
+				#ClusterSpeccs
+				cluster=cluster_1,
+				time = clustertime_1,
 
-                                        specific=string, 
-                                        grid_bias_voltage = 1,
+				specific=string, 
+				grid_bias_voltage = 1,
 
-                                        ## Timing 
-                                        time_start=timestart,
-                                        time_end = timeend,
-                                        time_grid = timegrid,
+				## Timing 
+				time_start=timestart,
+				time_end = timeend,
+				time_grid = timegrid,
 
-                                        ## Fixed for this Project
-                                        C= potential["C"],
-                                        A =  potential["A"], B= potential["B"],
-                                        l = potential["l"],
-                                        delta= potential["delta"],
+				## Fixed for this Project
+				C= potential["C"],
+				A =  potential["A"], B= potential["B"],
+				l = potential["l"],
+				delta= potential["delta"],
 
-                                        beta2L = set_bath_by_temp(T), beta2R=set_bath_by_temp(T),
+				beta2L = set_bath_by_temp(T), beta2R=set_bath_by_temp(T),
 
-                                        #Loop Variables
-                                        ## Bias
-                                        start_bias_voltage=b,                                        
-                                        end_bias_voltage=0.0,
+				#Loop Variables
+				## Bias
+				start_bias_voltage=b,                                        
+				end_bias_voltage=0.0,
 
-                                        ## Gate
-                                        start_gate_voltage=gate,
-                                        end_gate_voltage=gate,
+				## Gate
+				start_gate_voltage=gate,
+				end_gate_voltage=gate,
 
-                                        Vb=vb,
-                                        xshift = frank,
-                                        T = T, hbath_temp=T,
-                                        eta = en)      
+				Vb=vb,
+				xshift = frank,
+				T = T, hbath_temp=T,
+				eta = en)      
 
 
 # In[ ]:
@@ -392,7 +393,7 @@ project_onoff_rhox.put_runscript(); project_onoff_rhox.put_jobproject()
 # In[ ]:
 
 #External Parameters
-temp =[10, 293] 
+temp =[293] 
 env =[0.0, 0.04] 
 bias = [0, 0.2, 0.4]
 
@@ -403,7 +404,7 @@ states =[1,2]
 
 # In[ ]:
 
-timestart = 0; timeend= 1e7; timegrid=1e5; clustertime_2 = "16:00:00"; cluster_2="lima"
+timestart = 0; timeend= 1e8; timegrid=1e5; clustertime_2 = "24:00:00"; cluster_2="lima"
 
 
 # In[ ]:
@@ -526,6 +527,10 @@ project_switch = cluster.jobproject(name="switch", program=program_rrze, program
                                         pop_number=15, rhox_bool=1, plot_bool=1, meBND=6,meBND_small=6,xranges=2.0e0,
                                         medim1=60,medim0=60, timebool=0, potential_id=0, initialstate=1)
 
+project_switch_lowT = cluster.jobproject(name="switch_lowT", program=program_rrze, programprojectpath=path_rrze  , projectpath=projectpath,
+                                        mode=20, N=2000, summary_bool=1, performance_bool=1, pop_bool=1, coupling_bool=1,
+                                        pop_number=15, rhox_bool=1, plot_bool=1, meBND=6,meBND_small=6,xranges=2.0e0,
+                                        medim1=60,medim0=60, timebool=0, potential_id=0, initialstate=1)
 
 # In[10]:
 
@@ -537,7 +542,7 @@ project_switch_rhox = cluster.jobproject(name="switch_rhox", program=program_rrz
 
 # In[11]:
 
-project_switch_list =[project_switch, project_switch_rhox] 
+project_switch_list =[project_switch, project_switch_rhox, project_switch_lowT] 
 
 
 # In[12]:
@@ -603,6 +608,12 @@ for potential in configuration_dyn:
 # In[13]:
 
 project_switch.put_jobproject()
+
+for job in project_switch_lowT.joblist:
+    job.beta2L = 0.01
+    job.beta2R = 0.01
+
+project_switch_lowT.put_jobproject()
 
 
 # In[14]:
